369a546c530ac49e88d983db23758c90d903e8d7
AlexK <kulyamzin@gmail.com>
2020-10-07 01:22:10 +0300

    changed some styles

---
+++ Students/AlexK/project/src/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    message: 'hello VUE';
+  }
+  }
+</script>
+
+<style>
+
+</style>
--- Students/AlexK/project/src/index.js
+++ Students/AlexK/project/src/index.js
@@ -2,7 +2,12 @@ import './styles/css/style.css';
 import './styles/css/mStyle.css';
 
 import app from "./components";
-
-
-
 app();
+
+// import Vue from 'vue';
+// import Main from './Main.vue';
+
+// new Vue({
+//     render: h => h(Main),
+// }
+// ).$mount('#app')
