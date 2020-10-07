9c08f100b884735a0f5e1dd793a8d64460e8584b
Dementev Oleg <demeoleg@mail.ru>
2020-10-07 20:13:24 +0300

    Выполнил ДЗ к 4-ой лекции - папка /other/dz4 Выполнил ДЗ к 5-ой лекции - по видео с вебинара

--- Students/Oleg Dementev/Project/src/components/index.js
+++ Students/Oleg Dementev/Project/src/components/index.js
@@ -1,7 +1,38 @@
-import Basket from './basket';
-import Catalog from './catalog';
-
 export default () => {
-    let basket = new Basket();
-    let catalog = new Catalog(basket);
+    let app = new Vue({
+        el: '#app',
+        data: {
+            catalogItems: [],
+            basketItems: [],
+            showBasket: false,
+            catalogUrl: 'https://raw.githubusercontent.com/kellolo/static/master/JSON/catalog.json',
+            basketUrl: 'https://raw.githubusercontent.com/kellolo/static/master/JSON/basket.json'
+        },
+        methods: {
+            _get(url) {
+                return fetch(url).then(d => d.json())
+            },
+            add(item) {
+                let find = this.basketItems.find(el => el.productId == item.productId);
+                if (find) {
+                    find.amount++;
+                } else {
+                    let newItem = Object.assign({}, item, { amount: 1});
+                    this.basketItems.push(newItem);
+                }
+            },
+            remove(id) {
+                let find = this.basketItems.find(el => el.productId == id);
+                if (find.amount > 1) {
+                    find.amount--;
+                } else {
+                    this.basketItems.splice(this.basketItems.indexOf(find), 1);
+                }
+            },
+        },
+        mounted() {
+            this._get(this.catalogUrl).then(items => { this.catalogItems = items });
+            this._get(this.basketUrl).then(basket => { this.basketItems = basket.content });
+        }
+    });
 }
