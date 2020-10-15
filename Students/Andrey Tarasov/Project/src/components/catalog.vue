e5eec8c351c34092229a7c7033494e1b831136be
Tarasov_Andrey <astarasov79@mail.ru>
2020-10-15 18:44:06 +0300

    the build works with dev, but images don_t load

--- Students/Andrey Tarasov/Project/src/components/catalog.vue
+++ Students/Andrey Tarasov/Project/src/components/catalog.vue
@@ -1,34 +1,37 @@
 <template>
-  <div class="row" id="catalog">
-    <div class="col-10 offset-1 col-sm-6 offset-sm-0 col-md-4 col-lg-3 feturedItems">
-    <Item v-for="item of catalogItems" :key="item.productId" :item="item" />
-    </div>
+  <div id="catalog">
+      <div class="feturedItems">
+            <Item 
+                v-for="item of items" 
+                :key="item.productId"
+                :item="item"
+            />
+      </div>
   </div>
 </template>
 
 <script>
-// import Item from './item.vue'
+import Item from './item.vue'
 export default {
-  components: { Item },
-  data() {
-    return {
-      items: [],
-      showCatalog: true,
-      url: 'https://raw.githubusercontent.com/kellolo/static/master/JSON/catalog.json'
-    }
-  },
-
-  methods: {
-    get(url) {
-			return fetch(url).then(d => d.json())
+    components: { Item },
+    data() {
+        return {
+            items: [],
+            url: 'https://raw.githubusercontent.com/kellolo/static/master/JSON/catalog.json'
+        }
     },
-    
+    methods: {
+        _get(url) {
+            return fetch(url).then(d => d.json())
+        },
+    },
     mounted() {
-			this.get(this.url).then(items => {
-			this.items = items
-			})
-		},
-  }
+        this._get(this.url).then(items => { this.items = items });
+
+        console.log(this)
+        console.log(this.$parent)
+        console.log(this.$root)
+    }
 }
 </script>
 
